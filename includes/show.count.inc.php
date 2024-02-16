1465869d6ed8185e3f9ea7c33f592818743bcef2
jamesdank <94645949+jamesdank@users.noreply.github.com>
2024-02-16 15:54:28 -0600

    Update show.count.inc.php

--- includes/show.count.inc.php
+++ includes/show.count.inc.php
@@ -1,5 +1,5 @@
 <?php
     echo "
-        </br><center><b>This Recipe has been viewed {$count} times</b></center>
+        </br><center><b>This page has been viewed {$count} times</b></center>
     ";
 ?>
